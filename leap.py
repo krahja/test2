972ce6d324471f0ca5f5f1955525927336bf2e00
krahja <k.v.shuvalov@gmail.com>
2019-03-08 15:51:06 +0500

    f c

---
+++ leap.py
@@ -0,0 +1,24 @@
+import time, pyautogui
+from datetime import datetime
+from mss import mss
+from PIL import Image
+print('Leapforce industries {r}')
+print(pyautogui.position())
+while True:
+    time.sleep(55)
+    pyautogui.click(x=85,y=105)
+    time.sleep(5)
+    now = datetime.strftime(datetime.now(), "%H")
+    now = int(now)
+    if now >= 5 and now <= 23:
+        sct = mss()
+        name = 's.png'
+        f = sct.shot(output=name)
+        im = Image.open(name)
+        pix = im.load()
+        if pix[75,239][0] == 255:
+            print(pix[pyautogui.position()][0])
+            pyautogui.click(x=192,y=244)
+            #action
+            time.sleep(60*3)
+    #pyautogui.click(x=1135,y=862)
